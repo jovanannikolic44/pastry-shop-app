4fee8635ba1a91178e44bf636b12e2460bd23bfc
jovanannikolic44 <jovana.nikolic452@gmail.com>
2024-01-31 01:24:32 +0100

    Finish add function

--- src/app/components/add-new-item/add-new-item.component.ts
+++ src/app/components/add-new-item/add-new-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Item } from '../../models/Item';
 
 @Component({
   selector: 'app-add-new-item',
@@ -46,12 +47,62 @@ export class AddNewItemComponent implements OnInit {
   }
 
   saveClickedImage(image: string): void {
-    console.log(image)
+    this.choosenImage = image;
   }
 
   add(): void {
+    if(this.showCakesOrCookies == "cakes") {
 
-    this.itemId++;
-    localStorage.setItem('itemId', this.itemId.toString());
+      let indexToRemove = this.predefinedCakesImages.indexOf(this.choosenImage);
+      if(indexToRemove > -1) {
+        this.predefinedCakesImages.splice(indexToRemove, 1);
+      }
+
+      let allCakesString = localStorage.getItem('cakes');
+      let allCakes = allCakesString? JSON.parse(allCakesString) : [];
+
+      let newItem: Item = {
+        id: this.itemId.toString(),
+        name: this.name,
+        description: this.description,
+        price: this.price,
+        composition: this.composition.split(','),
+        type: 'torta',
+        imageUrl: this.choosenImage
+      };
+
+      allCakes.push(newItem);
+      localStorage.setItem('cakes', JSON.stringify(allCakes));
+
+      this.itemId++;
+      localStorage.setItem('itemId', this.itemId.toString());
+    }
+    else {
+      let indexToRemove = this.predefinedCookiesImages.indexOf(this.choosenImage);
+      if(indexToRemove > -1) {
+        this.predefinedCookiesImages.splice(indexToRemove, 1);
+      }
+
+      let allCookiesString = localStorage.getItem('cookies');
+      let allCookies = allCookiesString? JSON.parse(allCookiesString) : [];
+
+      let newItem: Item = {
+        id: this.itemId.toString(),
+        name: this.name,
+        description: this.description,
+        price: this.price,
+        composition: this.composition.split(','),
+        type: 'kolac',
+        imageUrl: this.choosenImage
+      };
+
+      allCookies.push(newItem);
+      localStorage.setItem('cookies', JSON.stringify(allCookies));
+
+      this.itemId++;
+      localStorage.setItem('itemId', this.itemId.toString());
+    }
+
+    this.message = "Dodavanje novog proizvoda je uspesno."
   }
 }
